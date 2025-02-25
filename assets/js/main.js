1181fac58e500907934f92b8e76a6420279ce990
m4rrix <mariacamana998@gmail.com>
2025-02-25 19:37:53 -0300

    cambio hash

--- assets/js/main.js
+++ assets/js/main.js
@@ -208,7 +208,7 @@
 				// Add state?
 					if (typeof addState != 'undefined'
 					&&	addState === true)
-						
+						history.pushState(null, null, '#');
 
 				// Handle lock.
 
@@ -333,9 +333,64 @@
 
 			});
 
-			
+			$window.on('hashchange', function(event) {
 
-		
+
+				// Si el hash es solo "#" o está vacío al cargar la página, quítalo de la URL
+				if (location.hash === '#' || location.hash === '') {
+					history.replaceState(null, null, location.pathname);
+				}
+
+
+				// Empty hash?
+					if (location.hash == ''
+					||	location.hash == '#') {
+
+						// Prevent default.
+							event.preventDefault();
+							event.stopPropagation();
+
+						// Hide.
+							$main._hide();
+
+					}
+
+				// Otherwise, check for a matching article.
+					else if ($main_articles.filter(location.hash).length > 0) {
+
+						// Prevent default.
+							event.preventDefault();
+							event.stopPropagation();
+
+						// Show article.
+							$main._show(location.hash.substr(1));
+
+					}
+
+			});
+
+		// Scroll restoration.
+		// This prevents the page from scrolling back to the top on a hashchange.
+			if ('scrollRestoration' in history)
+				history.scrollRestoration = 'manual';
+			else {
+
+				var	oldScrollPos = 0,
+					scrollPos = 0,
+					$htmlbody = $('html,body');
+
+				$window
+					.on('scroll', function() {
+
+						oldScrollPos = scrollPos;
+						scrollPos = $htmlbody.scrollTop();
+
+					})
+					.on('hashchange', function() {
+						$window.scrollTop(oldScrollPos);
+					});
+
+			}
 
 		// Initialize.
 
